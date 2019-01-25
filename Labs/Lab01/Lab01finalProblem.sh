2af4d81f2c60489855a3ce0200474bb6a6cdab72
brusselsproutshawty <sarah.colbert@colorado.edu>
2019-01-25 13:05:00 -0700

    added comments to shell script

--- Labs/Lab01/Lab01finalProblem.sh
+++ Labs/Lab01/Lab01finalProblem.sh
@@ -1,2 +1,4 @@
+#output to new file with only header line and columns 2-4
 cut -f 2-4 -d , PredPreyData.csv | grep time > Last10PredPrey.csv
+#add last 10 lines of data of columns 2-4 to previously made file
 cut -f 2-4 -d , PredPreyData.csv | tail >> Last10PredPrey.csv
